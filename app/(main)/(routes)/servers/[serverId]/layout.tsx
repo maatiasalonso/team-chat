babdb4df38ba0f151c65a4b65b2acd78afaa8f85
MatiasSM <matias.a.sanmartin@gmail.com>
2023-09-27 00:58:06 -0300

    fix: test

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -4,7 +4,7 @@ import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const ServerIdLayout = async ({
+export const ServerIdLayout = async ({
   children,
   params,
 }: {
@@ -31,13 +31,9 @@ const ServerIdLayout = async ({
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        <>
-          <ServerSidebar serverId={params.serverId} />
-        </>
+        <ServerSidebar serverId={params.serverId} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
   );
 };
-
-export default ServerIdLayout;
